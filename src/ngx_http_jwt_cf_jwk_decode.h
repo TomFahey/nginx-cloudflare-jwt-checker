63896c2445c33fad77aa7b9b48858ca73f0ce32a
Ricky Wu <rickywo@gmail.com>
2022-07-18 10:16:21 +1000

    refactor: fix compile error

--- src/ngx_http_jwt_cf_jwk_decode.h
+++ src/ngx_http_jwt_cf_jwk_decode.h
@@ -21,8 +21,8 @@
 #include <openssl/rsa.h>
 #include "ngx_http_jwt_cf_jwk_request.h"
 
-char *base64_urlsafe_to_standard(ngx_pool_t *pool, char* base64url);
-unsigned char *base64_decode(ngx_pool_t *pool, u_char* base64data, int* len);
+u_char *base64_urlsafe_to_standard(ngx_pool_t *pool, char* base64url);
+u_char *base64_decode(ngx_pool_t *pool, u_char* base64data, int** len);
 BIGNUM *bignum_base64_decode(ngx_pool_t *pool, u_char* base64bignum, int* len);
 u_char *jwk_to_pem_u_char(ngx_pool_t *pool, struct pubkey_t pubkey);
 
